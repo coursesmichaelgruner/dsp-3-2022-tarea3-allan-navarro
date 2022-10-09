81e7fdc4cf41c770bd90cf22b0e44ae4e2f5f369
Allan Navarro <anavarro3106@gmail.com>
2022-10-09 16:08:08 -0600

    Improve comments

--- prob1.py
+++ prob1.py
@@ -4,35 +4,36 @@ import sounddevice as sd
 import numpy as np
 import sys
 
-fs = 44100
-queue = []
-counter = 0
+fs = 44100 # sample rate
+samples = [] # queue to store samples
+read_samples = 0 # counter of read samples
 
 if len(sys.argv) != 2:
     print('need to specify delay D in ms')
     exit(1)
 
-D = int(sys.argv[1])
+D = int(sys.argv[1]) # delay in ms
 
 if D < 0:
     print('D must be >= 0')
     exit(1)
 
-delay_samples = (D/1000) * fs
+delay_samples = (D/1000) * fs # delay expressed in number of samples
 
 try:
     with sd.Stream(samplerate=fs, dtype='int16', latency='low', channels=1,blocksize=1) as stream:
         while True:
             read_available=stream.read_available
-            data,oflow = stream.read(read_available)
+            data,oflow = stream.read(read_available) # capture samples from stream
 
-            queue.append(data)
+            samples.append(data) # push samples to queue
 
-            if counter > delay_samples:
-                stream.write(queue.pop(0))
+            if read_samples > delay_samples:
+                stream.write(samples.pop(0)) # write samples to stream
             else:
+                # fill with zeros to avoid underrun errors
                 stream.write(np.zeros((100,1),dtype=np.int16))
-            counter += read_available
+                read_samples += read_available # increase the counter of samples read
 
 except KeyboardInterrupt:
     pass
